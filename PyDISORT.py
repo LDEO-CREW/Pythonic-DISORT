We have fixed the delta-M scaling but we do not know why (it works numerically). Also Stamnes' FORTRAN DISORT is mostly up and working.

PyDISORT.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy as sc
 from math import pi
 from numpy.polynomial import legendre
+from inspect import signature
 
 def PyDISORT(
     b_pos, b_neg, only_flux, NQuad, NLeg, Leg_coeffs_full, tau0, w0, mu0, phi0, I0, f=0, p_for_NT_with_g=(None,),
@@ -31,20 +32,21 @@ def PyDISORT(
      :Optional:
      - *u* (function) - Intensity function with arguments (tau, phi); the output is in the order (mu, tau, phi)
     """
-    Leg_coeffs = Leg_coeffs_full[:NLeg]
+    Leg_coeffs = Leg_coeffs_full[:NLeg].copy()
     
     # INPUT CHECKS
     # -----------------------------------------------------------
-    # The fractional scattering into peak must be 0<= and <1
+    # The Legendre coefficients must have magnitude <= 1
+    assert all(np.abs(Leg_coeffs) <= 1)
+    # Conditions on fractional scattering
     assert 0 <= f < 1
     # We require principal angles and a downward beam
     assert 0 < mu0 and mu0 < 1
     assert 0 <= phi0 and phi0 < 2 * pi
-    # We require NQuad to be >= 2, and even
-    # NLeg must obviously be >0
-    assert NQuad % 2 == 0
+    # Conditions on the number of quadrature angles (NQuad) and Legendre coefficients (NLeg)
     assert NQuad >= 2
-    assert NLeg > 0
+    assert NQuad % 2 == 0
+    # assert NLeg <= NQuad
 
     N = NQuad // 2
     # The following ensures that the BC inputs are of the correct shape
@@ -67,8 +69,7 @@ def PyDISORT(
 
     # Delta-M scaling; there is no scaling if f==0
     scale_tau = 1 - w0 * f
-    scale_beam = 1 + w0 * f
-    Leg_coeffs = (Leg_coeffs - f) / (1 - f)
+    Leg_coeffs = (Leg_coeffs - f) / (1 - f) * (2 * np.arange(NLeg) + 1)
     w0 *= (1 - f) / scale_tau
     tau0 *= scale_tau
 
@@ -82,13 +83,13 @@ def PyDISORT(
         
         # The magnitude of the asymmetry factor must be <1
         assert np.abs(g) < 1
-        # We check that the last Legendre coefficient in Leg_coeffs matches the inputed phase function
+        # We check that the last WEIGHTED Legendre coefficient in Leg_coeffs matches the inputed phase function
         assert np.isclose(
-            ((2 * (NLeg - 1) + 1) / 2)
+            (1 / 2)
             * integrate.quad(
                 lambda nu: p_for_NT(nu, g) * sc.special.eval_legendre(NLeg - 1, nu), -1, 1
             )[0],
-            Leg_coeffs[NLeg - 1] * (1 - f) + f,
+            Leg_coeffs[NLeg - 1] / (2 * (NLeg - 1) + 1) * (1 - f) + f,
         )
         
         # Delta-M scaled solution; no further corrections to the flux
@@ -100,7 +101,7 @@ def PyDISORT(
             mu_arr_pos, weights_mu,
             tau0, w0,
             mu0, phi0, I0, 
-            scale_tau, scale_beam,
+            scale_tau,
         )
 
         # NT corrections for the intensity
@@ -113,7 +114,7 @@ def PyDISORT(
             tilde_mathcal_B = lambda tau, mu_arr, phi: (
                 ((w0 * I0) / (4 * np.pi * (1 - f)))
                 * np.exp(-tau / mu0)[None, :, None]
-                * atleast_2d_back(p_for_NT_muphi(mu_arr, phi, -mu0, phi0, g))[:, None, :]
+                * atleast_2d_append(p_for_NT_muphi(mu_arr, phi, -mu0, phi0, g))[:, None, :]
                 / (mu_arr / mu0 + 1)[:, None, None]
             )
             tilde_u_star1_pos = (
@@ -141,7 +142,7 @@ def PyDISORT(
             mathcal_B = lambda tau, mu_arr, phi: (
                 ((w0 * I0) / (4 * np.pi))
                 * np.exp(-tau / mu0)[None, :, None]
-                * atleast_2d_back(p_truncated(mu_arr, phi, -mu0, phi0))[:, None, :]
+                * atleast_2d_append(p_truncated(mu_arr, phi, -mu0, phi0))[:, None, :]
                 / (mu_arr / mu0 + 1)[:, None, None]
             )
             u_star1_pos = (
@@ -173,5 +174,5 @@ def PyDISORT(
             mu_arr_pos, weights_mu,
             tau0, w0,
             mu0, phi0, I0, 
-            scale_tau, scale_beam,
+            scale_tau,
         )

subroutines.py
@@ -89,7 +89,6 @@ def generate_Ds(m, Leg_coeffs, mu_arr_pos, w0, ells, degree_tile):
     return D_pos, D_neg
 	
 
-# Generate the X term in the system of ODEs
 def generate_Xs(m, Leg_coeffs, w0, mu0, I0, mu_arr_pos, ells, degree_tile):
     """Generates the X term in the system of ODEs for each Fourier mode, see Section 3.2
     
@@ -125,14 +124,13 @@ def generate_Xs(m, Leg_coeffs, w0, mu0, I0, mu_arr_pos, ells, degree_tile):
     return X_pos, X_neg
 	
 	
-# Create the up and down flux functions
 def generate_flux_functions(
     I0, mu0, tau0,
     GC_pos, GC_neg, 
     eigenvals, N,
     B_pos, B_neg, 
     mu_arr_pos, weights_mu, 
-    scale_tau, scale_beam,
+    scale_tau,
 ): 
     """Generates the flux functions with respect to the radiative transfer equation
     
@@ -144,7 +142,6 @@ def generate_flux_functions(
      - *B_pos / neg* (vector) - Coefficients of the inhomogenity that correspond to positive / negative mu values
      - *mu_arr_pos / neg* (vector) - Positive / negative mu (quadrature) values
      - *scale_tau* (float) - Delta-M scale factor for tau
-     - *scale_beam* (float) - Delta-M scale factor for the direct beam
      
      
     :Output:
@@ -152,6 +149,14 @@ def generate_flux_functions(
      - *flux_down* (vector) - Flux function with argument tau for negative (downward) mu values
     """
     def flux_up(tau):
+        """Returns the magnitude of the upwards flux at the specified tau levels
+
+        :Input:
+        - *tau* - Optical depth levels
+
+        :Output:
+        - Magnitude of diffuse upwards flux, which is also the total upwards flux
+        """
         tau = scale_tau * np.atleast_1d(tau) # Delta-M scaling
         exponent = np.vstack(
             (
@@ -160,10 +165,21 @@ def generate_flux_functions(
             )
         )
         um = GC_pos @ np.exp(exponent) + B_pos[:, None] * np.exp(-tau[None, :] / mu0)
-        return np.squeeze(2 * pi * (mu_arr_pos * weights_mu) @ um)
+        return np.squeeze(2 * pi * (mu_arr_pos * weights_mu) @ um)[()]
 
     def flux_down(tau):
-        tau = scale_tau * np.atleast_1d(tau) # Delta-M scaling
+        """Returns the magnitude of the downwards fluxes at the specified tau levels
+
+        :Input:
+        - *tau* - Optical depth levels
+
+        :Output:
+        - Magnitude of diffuse downwards flux
+        - Magnitude of direct downwards flux
+        """
+        direct_beam = I0 * mu0 * np.exp(-tau / mu0)
+        
+        tau = scale_tau * np.atleast_1d(tau)  # Delta-M scaling
         exponent = np.vstack(
             (
                 eigenvals[:N, None] * tau[None, :],
@@ -171,18 +187,18 @@ def generate_flux_functions(
             )
         )
         um = GC_neg @ np.exp(exponent) + B_neg[:, None] * np.exp(-tau[None, :] / mu0)
-        return np.squeeze(
-            2 * pi * (mu_arr_pos * weights_mu) @ um
-            + scale_beam * I0 * mu0 * np.exp(-tau / mu0)
+        return (
+            np.squeeze(2 * pi * (mu_arr_pos * weights_mu) @ um)[()],
+            direct_beam,
         )
 
     return flux_up, flux_down
     
 
-# The following function is exactly NumPy's `atleast_2d` function but edited
+# The following function is exactly NumPy's `atleast_2d` function but altered
 # to add dimensions to the back of the shape tuple rather than to the front.
-# Documentation for `np.atleast_2d`: https://numpy.org/doc/stable/reference/generated/numpy.atleast_2d.html.
-def atleast_2d_back(*arys):
+# Documentation for `np.atleast_2d` taken from https://numpy.org/doc/stable/reference/generated/numpy.atleast_2d.html.
+def atleast_2d_append(*arys):
     """
     View inputs as arrays with at least two dimensions.
     Parameters